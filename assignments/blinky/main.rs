e7809cdc9c7334b173d1b62e062f7a2f4d41faac
Henk Dieter Oordt <henkdieter@oordt.net>
2021-03-30 17:01:38 +0200

    WIP on GDB config

---
+++ assignments/blinky/main.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+
+use hal::stm32 as pac;
+use stm32l4xx_hal as hal;
+
+// Contains all kinds of nice extension traits
+use hal::prelude::*;
+
+use core::panic::PanicInfo;
+use cortex_m_rt::{entry};
+
+// Contains definitions needed for defmt,
+// so we can print messages.
+// Also contains a panic handler,
+// so we don't need to add it here anymore
+// use assignments as _;
+
+#[entry]
+fn start() -> ! {
+    // Get a handle to te Peripherals
+
+    // Initialize LED pin as output
+    // Initialize button pun as input
+
+    // defmt::info!("Hello, world");
+    loop {
+        cortex_m_semihosting::hprintln!("HELLO WORLD");
+        // TODO Read input pin state, checking if button was pressed
+        // TODO 
+    }
+}
+
+#[panic_handler]
+fn panic(_ef: &PanicInfo) -> ! {
+    loop {}
+}
